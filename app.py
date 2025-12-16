65be7e5fbed54dd76885a937cb85a061db67017e
salmon11062006 <csalomo7@gmail.com>
2025-12-16 08:53:30 +0700

    a

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 DB_CONFIG = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'Salomo531',  # Change this
+    'password': 'Salomo531', 
     'database': 'hotelreservation'
 }
 
